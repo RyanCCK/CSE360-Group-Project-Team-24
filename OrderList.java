9ff0fa67a9403d3cf997b71ebb4876372f9fa1a0
RyanCCK <64030101+RyanCCK@users.noreply.github.com>
2022-11-02 20:26:15 -0700

    Update OrderList.java

--- OrderList.java
+++ OrderList.java
@@ -1,67 +1,136 @@
+package application;
+
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.io.*;
 
 //Singleton class that stores all orders in active memory
-public class OrderList
+public class OrderList 
 {
+	private static OrderList instance = null;	//There will only be 1 instance of an OrderList
+	private ArrayList<Order> orderList;
+	private static String filename = "database.txt";
+	private File file = new File(filename);
 	
-	private ArrayList orderList;
-	static String file = "";
-	
-	//Constructor
-	public OrderList()  throws IOException
+	//private constructor
+	private OrderList() throws ClassNotFoundException, IOException 
 	{
-		orderList = new ArrayList<Object>();	//Object will be Order
+		orderList = new ArrayList<Order>();	
+		if (!file.exists())
+		{
+			file.createNewFile();
+		}
+		loadOrderList();
 	}
 	
-	//Read OrderList from file
-	private void readOrderList() throws IOException 
+	//public getter 
+	public static OrderList getOrderList() throws ClassNotFoundException, IOException
 	{
-		FileInputStream fileInStream = new FileInputStream(file);
-		ObjectInputStream objIn = new ObjectInputStream(fileInStream);
+		if (instance == null)
+			instance = new OrderList();
+		
+		return instance;
 	}
 	
-	//Write OrderList to file
-	private void writeOrderList() throws IOException
+	//Load orderList from file
+	public void loadOrderList() throws IOException, ClassNotFoundException 
 	{
-		FileOutputStream fileOutStream = new FileOutputStream(file);
-		ObjectOutputStream objOut = new ObjectOutputStream(fileOutStream);
+		if (file.length() != 0)	//Verify that file is not empty before reading
+		{
+			FileInputStream fileIn = new FileInputStream(filename);
+			ObjectInputStream objIn = new ObjectInputStream(fileIn);
+			
+			orderList = (ArrayList<Order>) objIn.readObject();
+			
+			fileIn.close();
+			objIn.close();
+		}
+	}
+	
+	//Save orderList to file
+	private void saveOrderList() throws IOException
+	{
+		FileOutputStream fileOut = new FileOutputStream(filename);
+		ObjectOutputStream objOut = new ObjectOutputStream(fileOut);
+
+		objOut.writeObject(orderList);
+		
+		fileOut.close();
+		objOut.close();
 	}
 	
 	//Insert Order in the correct sorted position
-	public boolean insertOrder() 
+	public void insertOrder(Order order) throws IOException 
 	{
-		return true;
+		orderList.add(order);
+		Collections.sort(orderList);
+		saveOrderList();
 	}
 	
 	//Delete Order
-	public boolean deleteOrder()
+	public boolean deleteOrder(int orderID) throws IOException
 	{
-		return true;
+		//Find order in orderList with given orderID
+		boolean found = false;
+		int i=0;
+		while (!found && i<orderList.size())
+		{
+			if (orderList.get(i).id == orderID)
+			{
+				found = true;
+				orderList.remove(i);
+				Collections.sort(orderList);
+				saveOrderList();
+			}
+			else ++i;
+		}
+		return found;
+	}
+	
+	//for testing purposes
+	public void printOrderList()
+	{
+		for(int i=0; i<orderList.size(); ++i)
+		{
+			System.out.println("id: " + orderList.get(i).id);
+			System.out.println("number of pizzas: " + orderList.get(i).pizzas.length);
+			for (int j=0; j<orderList.get(i).pizzas.length; ++j)
+			{
+				System.out.print(orderList.get(i).pizzas[j] + "\t");
+			}
+			System.out.println("\n");
+		}
 	}
 	
 	public ArrayList getCustomerOrderList(int customerID)
 	{
-		ArrayList customerLinkedList = new ArrayList<Object>();
-		//get head node
-		int orderListSize = orderList.size();
-		//Add every order created by customer with customerID to the customer's order list
-		//while(orderList.)
-		return customerLinkedList;
+		return orderList;
 	}
 	
 	public ArrayList getCashierOrderList()
 	{
-		
+		return orderList;
 	}
 	
 	public ArrayList getChefOrderList()
 	{
-		
+		return orderList;
 	}
 	
 	public Object getOrder()
 	{
-		
+		return orderList;
+	}
+	
+	//Helper function to clear current contents of database file
+	private void clearFile() throws IOException
+	{
+		FileWriter fwOb = new FileWriter(filename, false); 
+        PrintWriter pwOb = new PrintWriter(fwOb, false);
+        pwOb.flush();
+        pwOb.close();
+        fwOb.close();
+		System.out.println("File contents cleared");
 	}
 }
