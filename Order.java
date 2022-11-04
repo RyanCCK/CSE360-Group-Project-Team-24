ddee60b6d2e828f1adc45a4a37130a76ec9b78ae
Fl4shdr1ve <103151468+Fl4shdr1ve@users.noreply.github.com>
2022-11-03 21:06:22 -0700

    Added new methods, instance variables, etc.

--- Order.java
+++ Order.java
@@ -1,29 +1,87 @@
 import java.io.Serializable;
-import java.util.Comparator;
+import java.lang.Comparable;
+import java.util.ArrayList;
+import java.time.LocalTime;
 
 public class Order implements Serializable, Comparable<Order>
 {
-	private static final long serialVersionUID = 1L;	
-	int id;
-	String[] pizzas;
+	public ArrayList<Pizza> pizzas;
+	
+	private static final long serialVersionUid= 1L;	
+	
+	public int id;
+	public double cost;
+	
+	private LocalTime pickupTime;
+	private String status;
+	private int customerID;
+	
 	
 	//Basic unfinished constructor of an Order, used for testing OrderList
-	public Order(int id, int size)
+	public Order(int orderID, int size, LocalTime time, int custID)
 	{
-		this.id = id;
-		pizzas = new String[size];
-		for (int i=0 ; i<size; ++i)
-		{
-			pizzas[i] = "Medium";
-		}
+		id = orderID;
+		pickupTime = time;
+		status = "Not Started";
+		customerID = custID;
+		cost = 0.00;
+		
+		pizzas = new ArrayList<Pizza>();
 	}
 
 	@Override
 	public int compareTo(Order o) {
-		if (this.id > o.id)
+		if (this.id> o.id)
 			return 1;
-		else if (this.id < o.id)
+		else if (this.id< o.id)
 			return -1;
 		else return 0;
-	}	
+	}
+	
+	public void calculateCost()
+	{
+		for(int i = 0; i < pizzas.size(); i++)
+		{
+			cost += pizzas.get(i).getPrice();
+		}
+	}
+	
+	public LocalTime getTime()
+	{
+		return pickupTime;
+	}
+	
+	public String getStatus()
+	{
+		return status;
+	}
+	
+	public int getCustID()
+	{
+		return customerID;
+	}
+	public void setTime(LocalTime time)
+	{
+		pickupTime = time;
+	}
+	
+	public void setStatus(String newStatus)
+	{
+		status = newStatus;
+	}
+	
+	public void addPizza(Pizza pizza)
+	{
+		pizzas.add(pizza);
+		this.calculateCost();
+	}
+	
+	public void removePizza(Pizza pizza)
+	{
+		pizzas.remove(pizza);
+		this.calculateCost();
+	}
 }
+
+
+
