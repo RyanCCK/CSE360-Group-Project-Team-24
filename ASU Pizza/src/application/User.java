a404cee37c53a4d87a2a8f37bf5216d03bbbb7ac
RyanCCK <64030101+RyanCCK@users.noreply.github.com>
2022-11-03 18:42:38 -0700

    Add files via upload

---
+++ ASU Pizza/src/application/User.java
@@ -0,0 +1,41 @@
+package application;
+
+public abstract class User
+{
+    protected String ASURITEID;
+    protected String password;
+    protected String name;
+
+    public abstract void login();
+    public abstract void logout();
+
+    public String getASURITEID()
+    {
+        return ASURITEID;
+    }
+
+    public String getPassword()
+    {
+        return password;
+    }
+
+    public String name()
+    {
+        return name;
+    }
+
+    public void setASURITEID(String newID)
+    {
+        ASURITEID = newID;
+    }
+
+    public void setPassword(String newPass)
+    {
+        password = newPass;
+    }
+
+    public void setName(String newName)
+    {
+        name = newName;
+    }
+}
