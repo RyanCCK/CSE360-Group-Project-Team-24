5c1560b0834a2e53c92ec1fa8f7e2433af40f985
RyanCCK <64030101+RyanCCK@users.noreply.github.com>
2022-11-06 16:09:44 -0700

    Create Accounts.java

---
+++ Accounts.java
@@ -0,0 +1,35 @@
+public class Accounts {
+	private static int databaseSize = 10;
+	private static String[] customerId = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"};
+	private static String[] customerPassword = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J"};
+	private static String[] employeeId = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"};
+	private static String[] employeePassword = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J"};
+	
+	public boolean verifyCustomerLogin(String id, String password)
+	{
+		boolean valid = false;
+		for (int i=0; i<databaseSize; ++i)
+		{
+			if (customerId[i].equals(id))
+			{
+				if (customerPassword[i].equals(password))
+					valid = true;
+			}
+		}
+		return valid;
+	}
+	
+	public boolean verifyEmployeeLogin(String id, String password)
+	{
+		boolean valid = false;
+		for (int i=0; i<databaseSize; ++i)
+		{
+			if (employeeId[i].equals(id))
+			{
+				if (employeePassword[i].equals(password))
+					valid = true;
+			}
+		}
+		return valid;
+	}
+}
