e9ec37a9b1265fbcad895ab000bfab5ac388d529
tytram <114836946+tytram@users.noreply.github.com>
2022-11-07 14:39:03 -0700

    Add files via upload

---
+++ Cashier.java
@@ -0,0 +1,32 @@
+
+public class Cashier extends User{
+
+	public Cashier(String asuriteID, String password, String name) 
+	{
+		super.ASURITEID = asuriteID;
+		super.password = password;
+		super.name = name;
+	}
+	
+	public void login() 
+	{
+		//login through data base
+	}
+	
+	public void logout()
+	{
+		//logout through database
+	}
+	
+	public void sendtoChef(Order order)
+	{
+		//send order details to chef method added later.
+		order.setStatus("Sent to Chef");
+	}
+	
+	public void markAsRecieved(Order order)
+	{
+		order.setStatus("Received!");
+	}
+	
+}
