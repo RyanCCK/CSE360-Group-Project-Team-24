08ecb47eee665fdb12e0b692725b86e89da58647
Adam-ASU <arkarman@asu.edu>
2022-11-11 16:09:03 -0700

    added Scene Change
    added a function to change scenes when the login button is clicked, need to add a check if valid credentials once accounts database is finished

--- ASU Pizza/src/application/LoginController.java
+++ ASU Pizza/src/application/LoginController.java
@@ -4,10 +4,14 @@ import java.net.URL;
 import java.util.Collection;
 import java.util.ResourceBundle;
 import javafx.scene.text.*;
+import javafx.stage.Stage;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ListView;
@@ -18,11 +22,30 @@ import javafx.scene.paint.Color;
 
 public class LoginController implements Initializable
 {
+	@FXML
+	private Button login;
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		// TODO Auto-generated method stub
 		
+	}
+	
+	@FXML
+	public Customer onCustLogin(ActionEvent event) throws IOException {
+		changeScene("CustomerView.fxml", event);
+		
+		return null;
+	}
+	
+	public void changeScene(String fxml, ActionEvent event) throws IOException {
+		Stage stage;
+		Scene scene;
+		Parent root = FXMLLoader.load(getClass().getResource(fxml));
+		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+		scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
 	}
 	
 }
--- ASU Pizza/src/application/Main.java
+++ ASU Pizza/src/application/Main.java
@@ -2,20 +2,30 @@ import java.io.IOException;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 
 
 public class Main extends Application {
+	public Stage primaryStage;
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("CustomerView.fxml"));
-			Scene scene = new Scene(root,400,400);
+			this.primaryStage = primaryStage;
+			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Login.fxml"));
+			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("CustomerView.fxml").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
+			//primaryStage.setResizable(false);
+			//primaryStage.setFullScreen(true);
+			Rectangle2D primScreenBounds = Screen.getPrimary().getVisualBounds();
+			primaryStage.setX((primScreenBounds.getWidth() - primaryStage.getWidth()) / 2);
+			primaryStage.setY((primScreenBounds.getHeight() - primaryStage.getHeight()) / 2);
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
