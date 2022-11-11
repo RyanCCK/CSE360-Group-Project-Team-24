7c00ec451fc72ed0ddac67cce75685d083f988a4
RyanCCK <64030101+RyanCCK@users.noreply.github.com>
2022-11-11 08:32:07 -0700

    Update Order.java

--- Order.java
+++ Order.java
@@ -1,41 +1,30 @@
 import java.io.Serializable;
 import java.lang.Comparable;
 import java.util.ArrayList;
+import java.util.Random;
 import java.time.LocalTime;
 
 public class Order implements Serializable, Comparable<Order>
 {
+	private static final long serialVersionUid= -124916025845113285L;	
+	Random rand = new Random();
+	
 	public ArrayList<Pizza> pizzas;
-	
-	private static final long serialVersionUid= 1L;	
-	
 	public int id;
 	public double cost;
 	
 	private LocalTime pickupTime;
 	private String status;
-	private int customerID;
+	private String customerID;
 	
-	
-	//Basic unfinished constructor of an Order, used for testing OrderList
-	public Order(int orderID, int size, LocalTime time, int custID)
+	public Order(String customerID)
 	{
-		id = orderID;
-		pickupTime = time;
-		status = "Not Started";
-		customerID = custID;
-		cost = 0.00;
-		
+		this.customerID = customerID;
 		pizzas = new ArrayList<Pizza>();
-	}
-
-	@Override
-	public int compareTo(Order o) {
-		if (this.id> o.id)
-			return 1;
-		else if (this.id< o.id)
-			return -1;
-		else return 0;
+		status = "Not Started";
+		cost = 0.00;
+		id = rand.nextInt(100000000);      //creates random 8 digit number
+        //TODO: Check for duplicate ID's
 	}
 	
 	public void calculateCost()
@@ -61,10 +50,11 @@ public class Order implements Serializable, Comparable<Order>
 		return id;
 	}
 
-	public int getCustID()
+	public String getCustID()
 	{
 		return customerID;
 	}
+	
 	public void setTime(LocalTime time)
 	{
 		pickupTime = time;
@@ -86,7 +76,13 @@ public class Order implements Serializable, Comparable<Order>
 		pizzas.remove(pizza);
 		this.calculateCost();
 	}
+	
+	@Override
+	public int compareTo(Order o) {
+		if (this.id> o.id)
+			return 1;
+		else if (this.id< o.id)
+			return -1;
+		else return 0;
+	}
 }
-
-
-
