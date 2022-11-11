480cc5d0ea4a7ea1fa2c37b52f6b2e0fb339ce0b
Jack Haley <103151468+Fl4shdr1ve@users.noreply.github.com>
2022-11-10 20:18:49 -0700

    Update Chef.java

--- Chef.java
+++ Chef.java
@@ -4,20 +4,6 @@ public class Chef extends User
 	public Chef(String asuriteID, String password, String name)	//constructor
 	{
 		super(asuriteID, password, name);
-        
-        //maybe instead of login/logout, we call the constructor 
-        // with just asurite id and password, then it searches 
-        //through the "database" to find a matching name, that is then displayed
-	}
-	
-	public void login()
-	{
-		//look through database to find user name and password
-	}
-	
-	public void logout()
-	{
-		
 	}
 	
 	public void markAsCooking(Order order)
