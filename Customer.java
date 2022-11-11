516796a348a3edac84446c056d87fb26527610b1
RyanCCK <64030101+RyanCCK@users.noreply.github.com>
2022-11-11 08:33:36 -0700

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -1,75 +1,28 @@
 import java.io.IOException;
-import java.time.LocalTime;
-import java.util.Random;
 
 
 public class Customer extends User
 {
-    private Order order = null;
-    private int orderID = -1;
-
-    OrderList instance;
+    public Order order;	//The order being created by the customer
+    OrderList orderList;
 
     public Customer(String asuriteID, String password, String name) throws ClassNotFoundException, IOException
     {
         super(asuriteID, password, name);
-
-        instance = OrderList.getOrderList();
+        order = new Order(ASURITEID);
+        orderList = OrderList.getOrderList();
     }
 
-    //getters
-    public Order getOrder() throws Exception
+    //getter
+    public Order getOrder() 
     {
-        if(order == null)
-        {
-            throw new Exception("Order never initialized");
-        }
-
         return order;
     }
-
-    public int getOrderID() throws Exception
-    {
-        if(orderID == -1)
-        {
-            throw new Exception("Order never initialized");
-        }
-
-        return orderID;
-    }
     
-    public void createOrder(int size, LocalTime time, int ASURITEID)
+    //Submit the customer's current order to the orderList
+    public void submitOrder() throws IOException
     {
-        Random rand = new Random();
-        orderID = rand.nextInt(100000000);      //creates random 8 digit number
-
-        //not sure what size does,
-        order = new Order(orderID, size, time, ASURITEID); 
-    }
-
-    public void sendOrder() throws IOException
-    {
-        instance.insertOrder(order);
-    }
-
-    public void deleteOrder() throws IOException
-    {
-        instance.deleteOrder(orderID);
-        order = null;
-        orderID = -1;
-    }
-
-    public boolean isOrderComplete(int orderID)
-    {  
-        Order check = instance.getOrder(orderID);
-        
-        if(check.getStatus().toUpperCase().equals("READY"))
-        {
-            order = null;   //resets customer order once complete
-            orderID = -1;
-            return true;
-        }
-
-        return false;
+    	orderList.insertOrder(order);
+        order = new Order(ASURITEID);	//reset the customer's current order 
     }
 }
