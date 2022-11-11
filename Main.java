172b596260799e0c89129d6c63aa0ce2cbde9de0
RyanCCK <64030101+RyanCCK@users.noreply.github.com>
2022-11-11 08:30:57 -0700

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,27 @@
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("CustomerView.fxml"));
+			Scene scene = new Scene(root,400,400);
+			scene.getStylesheets().add(getClass().getResource("CustomerView.fxml").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) throws IOException, ClassNotFoundException {
+		launch(args);		//JavaFX
+	}
+}
