2e6ab2070bba6aa020a8d297dec9fb6a4dff4003
Jack Haley <103151468+Fl4shdr1ve@users.noreply.github.com>
2022-11-13 19:24:33 -0700

    Update Main.java

--- ASU Pizza/src/application/Main.java
+++ ASU Pizza/src/application/Main.java
@@ -1,5 +1,4 @@
 import java.io.IOException;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Rectangle2D;
@@ -8,7 +7,6 @@ import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
-
 
 public class Main extends Application {
 	@Override
